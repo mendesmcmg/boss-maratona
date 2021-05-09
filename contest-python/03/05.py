b3ee26bca368a7a527edf48b44dc3775328f8e60
Clara Mendes <claraplanejamento@gmail.com>
2021-05-09 18:24:24 -0300

    :adhesive_bandage: fixes 05 solution

--- contest-python/03/05.py
+++ contest-python/03/05.py
@@ -1,11 +1,14 @@
 a, b = map(int, input().split(' '))
 res = 0
+temp = 0
 
 if a%b == 0:
     print(int(a/b))
 else:
-    if a > b:
-        res = b+(a-b)
-    elif b > a:
-        res = (b-a)+a
+    while b!=0:
+        res += a//b
+        a%=b
+        temp = a
+        a = b
+        b = temp
     print(res)
